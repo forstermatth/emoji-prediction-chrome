3b237313e1d66c9a8702f88cc79bed76217ad92a
Ash Whitear <ashwhitear@gmail.com>
2015-10-12 15:37:34 +1000

    Minor changes

--- popup.js
+++ popup.js
@@ -1,10 +1,11 @@
 
-var html_string= "<html><body>Hello world</body></html>";
+var html_string= "<html><body></body></html>";
 var iframe = document.createElement("iframe");
 
 iframe.setAttribute("id", "emoji_frame");
 iframe.setAttribute("scrolling", "No");
 iframe.setAttribute("frameborder", "0");
+
 
 iframe.src = "data:text/html;charset=utf-8," + escape(html_string);
 document.body.appendChild(iframe);
